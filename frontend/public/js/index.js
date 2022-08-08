b4038c70cfdf070227fe2cddb05ed57d403f945c
yannick42 <yannick4283@hotmail.com>
2022-08-08 21:37:41 +0200

    update tabs

--- frontend/public/js/index.js
+++ frontend/public/js/index.js
@@ -1,9 +1,9 @@
 
 // Game states
 let game = {
-	currentPlayer: 'north',
+  currentPlayer: 'north',
   scores: {
-  	north: 0,
+    north: 0,
     east: 0,
     south: 0,
     west: 0
@@ -12,7 +12,7 @@ let game = {
 
 // Players
 let players = {
-	north: {},
+  north: {},
   east: {},
   south: {},
   west: {}
@@ -37,26 +37,26 @@ const allCards = colors.flatMap(color => {
 function distributeCardDeck(cards) {
   cards.forEach((card, i) => {
     let currentPlayer = players[playingOrder[i%playingOrder.length]];
-    if(currentPlayer.cards?.length)	{
+    if(currentPlayer.cards?.length)  {
       currentPlayer.cards.push(card);
     } else {
       currentPlayer.cards = [card];
     }
   });
-	playingOrder.map(player => players[player].cards.sort(orderFn))
+  playingOrder.map(player => players[player].cards.sort(orderFn))
 }
 
 // sort cards
 let orderFn = (card1, card2) => {
-	if(colors.indexOf(card1[0]) === colors.indexOf(card2[0])) {
-  	return ordered_numbers.indexOf(card1.slice(1)) > ordered_numbers.indexOf(card2.slice(1)) ? 1 : -1;
- 	}
+  if(colors.indexOf(card1[0]) === colors.indexOf(card2[0])) {
+    return ordered_numbers.indexOf(card1.slice(1)) > ordered_numbers.indexOf(card2.slice(1)) ? 1 : -1;
+   }
   return colors.indexOf(card1[0]) > colors.indexOf(card2[0]) ? 1 : -1;
 }
 
 function displayPlayerCards(player) {
-	players[player].cards.map(card => {
-  	let domElem = document.getElementById(player);
+  players[player].cards.map(card => {
+    let domElem = document.getElementById(player);
     let newCard = document.createElement('span');
     newCard.className = 'card';
     newCard.innerText = card;
@@ -66,22 +66,22 @@ function displayPlayerCards(player) {
 }
 
 function main() {
-	const shuffledCards = allCards.sort(() => Math.random() - 0.5);
+  const shuffledCards = allCards.sort(() => Math.random() - 0.5);
   
   // give cards
-	distributeCardDeck(shuffledCards);
+  distributeCardDeck(shuffledCards);
   
-	// afficher les cartes distribuées à chaque joueurs
-	playingOrder.map(player => displayPlayerCards(player));
+  // afficher les cartes distribuées à chaque joueurs
+  playingOrder.map(player => displayPlayerCards(player));
   
   let i = 0;
   // game loop
   while(true) {
-  	
+    
     i++;
     
     if(i >= 13) {
-    	// fin de pli
+      // fin de pli
       // TODO
       break;
     }
